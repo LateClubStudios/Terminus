77880e20143253c7ca1fe8d132e7df7943f295ab
paradox-glitch <35745554+paradox-glitch@users.noreply.github.com>
2018-11-29 15:27:15 +0000

    EndDay-2NOV18
    Latest copy of unity, bugs fixed inculde players box drag, and the custom cursor.

---
+++ Assets/Scripts/DragObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragObject : MonoBehaviour
+{
+    public GameObject item;
+    public GameObject player;
+	bool dragging = false;
+
+	private void Update()
+    {
+		if (Input.GetKeyDown ("e")) {
+			Debug.Log ("Object grabbed");
+			item.transform.parent = player.transform;
+		} else if (Input.GetKeyUp ("e")){
+			Debug.Log ("Object ungrabbed");
+			item.transform.parent = null;
+		}
+			
+    }
+		
+}
+
+
